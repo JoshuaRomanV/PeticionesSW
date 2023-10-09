b8b6d4a4f66185944e682197dd9889035dcac6ec
callarfaniv <villafrancac@hotmail.com>
2023-10-08 21:33:42 -0600

    feat: Tarea con más campos(titulo, descripcion y fecha de entrega)

--- src/App.jsx
+++ src/App.jsx
@@ -2,43 +2,88 @@ import { useState } from "react";
 import "./App.css";
 import AddTaskButton from "./components/AddTaskButton";
 import CancelButton from "./components/CancelButton.jsx";
-import { Alert, Modal } from "antd";
+import { Alert, Button, DatePicker, Form, Input, Modal, Space } from "antd";
 import TextArea from "antd/es/input/TextArea";
 
 function App() {
-    const [isModalOpen, setIsModalOpen] = useState(false)
-    const [task, setTask] = useState("")
     const [tasks, setTasks] = useState([])
-    const [isEmpty, setIsEmpty] = useState(false)
+    const [isModalOpen, setIsModalOpen] = useState(false)
+    const [form] = Form.useForm()
 
     const showModal = () => {
         setIsModalOpen(true);
     };
 
-    const handleOk = () => {
-        if (task != "") {
-            setTasks([...tasks, task])
-            setTask("")
-            setIsEmpty(false)
-            setIsModalOpen(false);
-        } else {
-            setIsEmpty(true)
-        }
-    };
-
     const handleCancel = () => {
-        setIsEmpty(false)
         setIsModalOpen(false);
         console.log(tasks)
     };
 
+    const onFinish = (values) => {
+        console.log('Success:', values);
+        const task = {
+            title: values.title,
+            description: values.description,
+            date: values.date,
+            completed: false
+        }
+        setTasks([...tasks, task])
+        setIsModalOpen(false)
+        form.resetFields()
+    };
+
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
+
+    const onReset = () => {
+        form.resetFields()
+    }
+
     return (
         <>
             <AddTaskButton onClick={showModal} />
-            <Modal title="Agregar tarea" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-                <TextArea onChange={event => setTask(event.target.value)} value={task} />
-                <p></p>
-                {isEmpty && (<Alert message="No es posible agregar tareas vacías" type="error" closable />)}
+            <Modal title="Agregar tarea" open={isModalOpen} onCancel={handleCancel} footer={false}>
+                <Form
+                    initialValues={{ remember: true }}
+                    layout="vertical"
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    form={form}
+                    autoComplete="off"
+                >
+                    <Form.Item
+                        label="Titulo"
+                        name="title"
+                        rules={[{ required: true, message: 'Por favor asigna un titulo a la tarea!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        label="Descripción"
+                        name="description"
+                        rules={[{ required: true, message: 'Por favor asigna una descripción a la tarea!' }]}
+                    >
+                        <TextArea />
+                    </Form.Item>
+                    <Form.Item
+                        label="Fecha de entrega"
+                        name="date"
+                        rules={[{ required: true, message: 'Por favor asigna una fecha de entrega a la tarea!' }]}
+                    >
+                        <DatePicker />
+                    </Form.Item>
+                    <Form.Item>
+                        <Space>
+                            <Button type="primary" htmlType="submit">
+                                Guardar
+                            </Button>
+                            <Button htmlType="reset" onClick={onReset}>
+                                Limpiar
+                            </Button>
+                        </Space>
+                    </Form.Item>
+                </Form>
             </Modal>
         </>
     );
