8e63b7af5a66cd34f177af54965e4b404c661836
MementoMori2080 <tristan.m.p@hotmail.com>
2023-10-13 16:35:18 -0600

    Funcionamiento botones eliminar y completar tarea

--- src/pages/ViewListTask.jsx
+++ src/pages/ViewListTask.jsx
@@ -1,106 +1,113 @@
+import React from "react";
 import { Divider, Space, Card, Row, Col, Button, Image, Input } from "antd";
 import { useEffect, useState } from "react";
 import "./StyleListTask.css";
 import { AiOutlineSearch, AiOutlineCheckCircle } from "react-icons/ai";
-
 import { TiDeleteOutline } from "react-icons/ti";
 
-// ... Importaciones y código anterior
-
 export default function ViewListTask(props) {
-    const [dataTask, setDataTask] = useState([]);
-    const [noData, setNoData] = useState(true);
-    const [searchText, setSearchText] = useState("");
-    const isNewTaskAdded = props.dataTask.length > dataTask.length;
+  const [dataTask, setDataTask] = useState([]);
+  const [noData, setNoData] = useState(true);
+  const [searchText, setSearchText] = useState("");
+  const isNewTaskAdded = props.dataTask.length > dataTask.length;
 
-    useEffect(() => {
-        function getTask() {
-            if (isNewTaskAdded) {
-                setDataTask(props.dataTask);
-            }
-        }
+  useEffect(() => {
+    function getTask() {
+      if (isNewTaskAdded) {
+        setDataTask(props.dataTask);
+      }
+    }
 
-        function checkData() {
-            if (props.dataTask.length === 0) {
-                setNoData(true);
-            } else {
-                setNoData(false);
-            }
-        }
+    function checkData() {
+      if (props.dataTask.length === 0) {
+        setNoData(true);
+      } else {
+        setNoData(false);
+      }
+    }
 
-        getTask();
-        checkData();
-    }, [props.dataTask, isNewTaskAdded]);
+    getTask();
+    checkData();
+  }, [props.dataTask, isNewTaskAdded]);
 
-    const filteredData = dataTask.filter((task) =>
-        task.title.toLowerCase().includes(searchText.toLowerCase())
-    );
+  const handleDeleteTask = (index) => {
+    props.handleDeleteTask(index);
+    window.location.reload();  // Recargar la página después de borrar la tarea
+  };
 
-    return (
-        <Row className="contentTask">
-            <Input
-                placeholder="Buscar por título"
-                value={searchText}
-                onChange={(e) => setSearchText(e.target.value)}
-                prefix={<AiOutlineSearch />} // Agregar un ícono de búsqueda
-                style={{ width: "60%", height: 50 }} // Ajustar el ancho del Input
-            />
+  const filteredData = dataTask.filter((task) =>
+    task.title.toLowerCase().includes(searchText.toLowerCase())
+  );
 
-            <Col span={24}>
-                <h1 className="title">Listado de mis tareas :</h1>
-            </Col>
-            <Col span={24}></Col>
-            {noData ? (
-                <Col span={24}>
-                    <h2>Por el momento no tienes tareas registradas</h2>
-                    <Image
-                        className="imgNoData"
-                        src="https://media2.giphy.com/media/WtOkaikiwaR87ZvAFH/giphy.gif?cid=6c09b95208wke0ehfiztsfqask4ufaivrsz6jp52ni7gntgi&ep=v1_gifs_search&rid=giphy.gif&ct=g"
-                    />
-                </Col>
-            ) : null}
-            {filteredData.length === 0 ? ( // Agregar esta condición
-                <Col span={24}>
-                    <h2>Sin tareas encontradas</h2>
-                </Col>
-            ) : null}
-            {filteredData.map((task, index) => {
-                return (
-                    <Col
-                        className="alingCard"
-                        xs={{ span: 24 }}
-                        lg={{ span: 10, offset: 1 }}
-                        key={index}
-                    >
-                        <Card className="cardTask">
-                            <p className="titleTask">{task.title}</p>
-                            <Divider />
-                            <p>{task.description}</p>
-                            <p>Fecha de entrega: {task.date}</p>
-                            <Space>
-                                <Button
-                                    className="btnTask"
-                                    type="primary"
-                                    shape="circle"
-                                    size="large"
-                                    danger
-                                >
-                                    <TiDeleteOutline size={40} />
-                                </Button>
-                                <Button
-                                    className="btnTask"
-                                    type="primary"
-                                    shape="circle"
-                                    size="large"
-                                    color="blue"
-                                >
-                                    <AiOutlineCheckCircle size={40} />
-                                </Button>
-                            </Space>
-                        </Card>
-                    </Col>
-                );
-            })}
-        </Row>
-    );
+  return (
+    <Row className="contentTask">
+      <Input
+        placeholder="Buscar por título"
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+        prefix={<AiOutlineSearch />}
+        style={{ width: "60%", height: 50 }}
+      />
+
+      <Col span={24}>
+        <h1 className="title">Listado de mis tareas :</h1>
+      </Col>
+      <Col span={24}></Col>
+      {noData ? (
+        <Col span={24}>
+          <h2>Por el momento no tienes tareas registradas</h2>
+          <Image
+            className="imgNoData"
+            src="https://media2.giphy.com/media/WtOkaikiwaR87ZvAFH/giphy.gif?cid=6c09b95208wke0ehfiztsfqask4ufaivrsz6jp52ni7gntgi&ep=v1_gifs_search&rid=giphy.gif&ct=g"
+          />
+        </Col>
+      ) : null}
+      {filteredData.length === 0 ? (
+        <Col span={24}>
+          <h2>Sin tareas encontradas</h2>
+        </Col>
+      ) : null}
+      {filteredData.map((task, index) => {
+        return (
+          <Col
+            className="alingCard"
+            xs={{ span: 24 }}
+            lg={{ span: 10, offset: 1 }}
+            key={index}
+          >
+            <Card className="cardTask">
+              <p className="titleTask">{task.title}</p>
+              <Divider />
+              <p>{task.description}</p>
+              <p>Fecha de entrega: {task.date}</p>
+              <Space>
+                <Button
+                  className="btnTask"
+                  type="primary"
+                  shape="circle"
+                  size="large"
+                  danger
+                  onClick={() => handleDeleteTask(index)}
+                >
+                  <TiDeleteOutline size={40} />
+                </Button>
+                {!task.completed && (
+                  <Button
+                    className="btnTask"
+                    type="primary"
+                    shape="circle"
+                    size="large"
+                    color="blue"
+                    onClick={() => props.handleCompleteTask(index)}
+                  >
+                    <AiOutlineCheckCircle size={40} />
+                  </Button>
+                )}
+              </Space>
+            </Card>
+          </Col>
+        );
+      })}
+    </Row>
+  );
 }
